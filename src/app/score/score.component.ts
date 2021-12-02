272ad152db07ff6f38e678841caad6306a87c2c6
Dan Lee <thejamootz@gmail.com>
2021-12-02 12:33:44 -0500

    Add files via upload

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -9,6 +9,7 @@ import { ScoreService } from './score.service';
 })
 export class ScoreComponent implements OnInit {
   //data: Data = { events: [{ competitions: [{ competitors: [{ linescores: [{}] }] }] }] };
+<<<<<<< Updated upstream
   data: GameData = { events: [{ competitions: [{ competitors: [{ linescores: [{}, {}, {}, {}, {}, {}] }] }] }] };
 
 
@@ -25,3 +26,48 @@ export class ScoreComponent implements OnInit {
 
 
 }
+=======
+  data: GameData =
+    {
+      events:
+        [date: '',
+          { links: [{ href: '' }] },
+          { status: { displayClock: '', period: '', type: { detail: '', completed: true } } },
+          {
+            competitions:
+              [{ series: { summary: '' }],
+            notes: [{ headline: '' }],
+            headlines: [{}],
+            venue: { fullName: '', address: { city: '', state: '' } },
+            broadcasts: [{ market: '', names: [{}] }],
+            competitors: [{
+              homeAway: 'home', score: '42', linescores: [{ value: '' }],
+              team: { displayName: '' }, records: [{ summary: '' }], 
+              leaders: [{ shortDisplayName: '', 
+                  leaders: [{ displayValue: '', 
+                      athlete: { displayName: '' } }] 
+                        }]
+            }]]
+          }}};
+
+events = this.data.events;
+linescores = this.data.events[0].competitions[0].competitors[0].linescores;
+
+
+//score = this.data.events[0].competitions[0].competitors[0].score;
+constructor(private service: ScoreService) { };
+
+ngOnInit(): void {
+  this.service.getData().subscribe(response => {
+    let testingThis: GameData = response;
+    console.log(testingThis);
+    console.log(testingThis.events[0].competitions[0].venue.fullName);
+    //      console.log(testingThis.events[0])
+    //      console.log('events[0]' + testingThis.events[0])      
+
+    //      console.log('this is the returned data ' + testingThis.events[0].competitions[0].competitors[0].score);
+
+  }
+    
+  )}}
+>>>>>>> Stashed changes
